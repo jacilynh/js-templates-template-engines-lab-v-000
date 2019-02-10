2b4b3d4be7a1c611dcec5437fe38a3b78f168136
Jacilyn Hayden <jacihayden@gmail.com>
2019-02-10 03:49:34 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+// Must have a form with an id of post-form
+
+// index, functions, createPost, builds the proper templates
+
+// index, functions, postComment, builds the proper templates
+
+// index, templates, has a comments template: Must have a lodash template with an id of 'comments-template'
+
+// index, templates, has a comment template: Must have a lodash template with an id of 'comment-template'
+
+// index, templates, has a page template: Must have a lodash template with an id of 'page-template'
+
+// index, templates, has a post template: Must have a lodash template with an id of 'post-template'
